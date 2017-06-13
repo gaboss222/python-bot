d6b75628a9c2d1efe3e05e64422f8f07663517dc
florian.fasmeyer <florian.fasmeyer@he-arc.ch>
2017-06-13 23:37:31 +0200

    Comment fix and add username recovery

--- tictactoeBot.py
+++ tictactoeBot.py
@@ -9,34 +9,50 @@ import logging
 
 description = """Python bot project by Florian Fasmeyer & Gabriel Grigri."""
 bot = commands.Bot(command_prefix='!', description=description)
+
 playPlayer1 = False
 isPlaying = False
 playVSPC = False
-log.setLevel(logging.DEBUG)
-log.addHandler(logging.StreamHandler())
+
 board = 0
+player1Name = '';
+player2Name = '';
+
 player1 = ':x:'
 player2 = ':o:'
+
+log.setLevel(logging.DEBUG)
+log.addHandler(logging.StreamHandler())
 #logging.basicConfig(level=logging.INFO)
 
+@bot.command(pass_context=True)
+async def myName is(ctx, member: discord.Member = None):
+    if member is None:
+        member = ctx.message.author
 
+    await bot.say('Your name is {0}'.format(member.name))
 
-@bot.command(description='Decide who to play')
+@bot.command(description='Play TicTacToe against another player.')
 async def playWithPlayer():
-    """Play with playerName"""
+    """Play TicTacToe against another player."""
     global isPlaying
+    global bot
+    global player1Name
     global playPlayer1
     if isPlaying is False : #and len(name)==0:
         await startGame()
         isPlaying = True
         playPlayer1 = True
-        await bot.say('Joueur 1 : ')
+        player1Name = 'haha'
+
+        await bot.say()
+        await bot.say(player1Name)
     else:
-        await bot.say('Une partie est en cours')
- 
-@bot.command(description='Decide who to play')
+        await bot.say('Une partie est en cours.')
+
+@bot.command(description='Play TicTacToe against the PC.')
 async def playWithPC():
-    """Play with PC"""
+    """Play TicTacToe against the PC."""
     global isPlaying
     global playPlayer1
     global playVSPC
@@ -47,18 +63,19 @@ async def playWithPC():
         playVSPC = True
         await bot.say('Joueur 1 : ')
     else:
-        await bot.say('Une partie est en cours')
- 
-@bot.command(description='Stop the game')
+        await bot.say('Une partie est en cours.')
+
+@bot.command(description='Stop the current game.')
 async def stop():
-    """ Stop the current game"""
+    """Stop the current game."""
     global isPlaying
     isPlaying = False
+    await bot.say('Fin de la partie.')
 
-@bot.command(description='Play a game of tictactoe')
+@bot.command(description='Make a move on the TicTacToe board (after starting a game).')
 async def move(*miniToe : int):
-    """Play a game of TicTacToe with the bot."""
-    
+    """Make a move on the TicTacToe board (after starting a game)."""
+
     global isPlaying
     winner = False
     global player1
@@ -66,11 +83,12 @@ async def move(*miniToe : int):
     global playPlayer1
     global playVSPC
     boardFull = False
+
     if isPlaying:
         if not winner:
             if not playVSPC:
-                    if len(miniToe)>0 and await isValid(miniToe[0]):          
-                            move = miniToe[0]                      
+                    if len(miniToe)>0 and await isValid(miniToe[0]):
+                            move = miniToe[0]
                             if playPlayer1:
                                 if await movePlayer(board, player1, move):
                                     await draw()
@@ -96,7 +114,7 @@ async def move(*miniToe : int):
                                             playPlayer1 = True
                                             await bot.say('Joueur 1 :')
                     else:
-                        await bot.say('Entrez un nombre (1-9)')
+                        await bot.say('Entrez un nombre de 1 à 9.')
             else:
                 if len(miniToe)>0:
                             move = miniToe[0]
@@ -119,28 +137,28 @@ async def move(*miniToe : int):
                                             elif await isBoardFull(board):
                                                 boardFull = True
                 else:
-                    await bot.say('Entrez un nombre (1-9)')
+                    await bot.say('Entrez un nombre de 1 à 9.')
     else:
-        await bot.say("Aucune partie en cours")     
-        
+        await bot.say("Aucune partie en cours.")
+
     if winner or boardFull:
-        await bot.say('Partie finie')
+        await bot.say('Fin de la partie.')
         isPlaying = False
         playVSPC = False
 
-        
+
 async def isValid(move):
-    """Is valid ?"""
+    """Return true if input within [1;9]."""
     if move >= 1 and move <= 9:
         return True
     else:
         return False
- 
+
 
 async def getMoveBot():
-    """Move from the bot"""
+    """Move from the bot."""
     global player2
-    #On test le moove du bot sur une copy du board
+    #On test le move du bot sur une copy du board
     #Si le moove fait du bot un gagnant, retourne ce moove (i)
     #Sinon, test si le player peut gagner au prochain moove, et retourne ce dernier pour le contrer
     #Sinon, centre, puis corners, puis random
@@ -167,15 +185,15 @@ async def getMoveBot():
             return i
     for i in range(7,9,2):
         if await isSpaceFree(board, i):
-            return i   
+            return i
     #Sinon, nombre random
     i = random.randint(1, 10)
     while not await isSpaceFree(boardCopy, i):
         i = i + 1
-    return i  
-   
-    
-    
+    return i
+
+
+
 async def getBoardCopy(b):
      # Make a duplicate of the board list and return it the duplicate.
      boardCopy = []
@@ -184,16 +202,17 @@ async def getBoardCopy(b):
          boardCopy.append(i)
 
      return boardCopy
- 
+
 
 async def movePlayer(b, player, move):
-    """Place move on the board"""
+    """Place move on the board.""" #Much wow!
 
     if await isValid(move) and (await isSpaceFree(b, move)):
         b[move] = player
         return True
 
     else:
+        await bot.say('Position déjà prise, essayez-en une autre.')
         return False
 
 
@@ -226,7 +245,7 @@ async def hasWon(board, player):
            board[3] == board[5] == player and board[5] == board[7]):
             return True
 
-          
+
 
 async def initBoard():
     """Initialize the board"""
@@ -234,14 +253,14 @@ async def initBoard():
     global board
     board = [':white_medium_square:'] * 10
 
-    
+
 async def isBoardFull(b):
         """Return true if the board is full"""
         for i in range(1, 10):
             if await isSpaceFree(b, i):
                 return False
-        return True     
-        
+        return True
+
 async def isSpaceFree(b, position):
     """Is the place is free ?"""
 
